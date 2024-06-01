bd23e90ee85dff8bcffdbf7b72161fb24b64a6fa
carloso0114 <carlossospinag@gmail.com>
2024-06-01 03:03:52 -0500

    setup NotesList component to fetch notes

---
+++ grades/src/components/NotesList.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+
+const NotesList = () => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetchNotes()
+  }, []);
+
+  const fetchNotes = async () => {
+    try {
+      const response = await fetch('/api/notas');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      setNotes(data.notas);
+    } catch (error) {
+      console.error('Error fetching notes:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Student Notes</h1>
+      <ul>
+        {notes.map(note => (
+          <li key={note.id}>
+            {note.estudiante } - {note.materia}: {note.notaFinal + " "} -
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default NotesList;
--- grades/vite.config.js
+++ grades/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Proxy all requests to the backend server
+      '/api': 'http://localhost:3000',
+    }
+  }
 })
